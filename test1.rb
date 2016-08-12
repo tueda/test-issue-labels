f4a771ec96f7d875c50a91c95415fe0d3d916130
Takahiro Ueda <tueda@nikhef.nl>
2016-08-12 17:33:21 +0200

    Use only assert()

--- test1.rb
+++ test1.rb
@@ -2,9 +2,9 @@ require 'test/unit'
 
 class TestSample < Test::Unit::TestCase
   def test_foo
-    assert_true(1 == 1)
+    assert(1 == 1)
   end
   def test_bar
-    assert_equal(1, 1)
+    assert(2 == 2)
   end
 end
