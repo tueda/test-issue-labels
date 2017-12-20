48aa1118e042b092330f8c6b09d6d897d0516cda
Takahiro Ueda <tueda@nikhef.nl>
2017-12-20 17:59:25 +0100

    Test GZIP transfer mode

---
+++ rundec.rb
@@ -0,0 +1,13 @@
+class Rundec < Formula
+  desc "Running and decoupling of the QCD coupling constant and quark mass"
+  homepage "https://www.ttp.kit.edu/preprints/2017/ttp17-011"
+  url "https://www.ttp.kit.edu/_media/progdata/2017/crundec3.tar.gz"
+  sha256 "f21525db29df5c4852bdcfef0f48bc0631abf620d8103645cfa290c2532dbbd0"
+
+  def install
+    system "ls", "-R", "."
+  end
+
+  test do
+  end
+end
