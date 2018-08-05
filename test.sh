7a3a12ba55e0edae424635fd3801be503b9a130d
Takahiro Ueda <tueda@st.seikei.ac.jp>
2018-08-05 20:58:29 +0900

    Initial commit

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+go_to_column() {
+  local column="$1"
+  printf "\x1B[%dG" $(( $column + 1 ))
+}
+
+clear_to_end_of_line() {
+  printf "\x1B[K"
+}
+
+printf "abcdef"
+go_to_column 2
+printf "34"
+printf "\n"
+
+printf "abcdef"
+go_to_column 2
+printf "34"
+printf "\n"
+
+printf "abcdef"
+go_to_column 2
+printf "34"
+printf "\n"
