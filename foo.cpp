a7681494d3e1cbdff6b2fe7536a45e998a8c2f50
Takahiro Ueda <tueda@st.seikei.ac.jp>
2020-04-24 17:20:18 +0900

    Reproduced!

---
+++ foo.cpp
@@ -0,0 +1,30 @@
+#include <fstream>
+#include <iostream>
+#include <typeinfo>
+
+void print(std::ifstream& ifs);
+
+int main() {
+  std::ifstream ifs("test.txt");
+  std::streampos pos;
+
+  print(ifs);
+  print(ifs);
+  pos = ifs.tellg();
+  std::cout << "save the pos : " << pos << std::endl;
+  print(ifs);
+  print(ifs);
+  print(ifs);
+  ifs.seekg(pos);
+  std::cout << "load the pos : " << pos << std::endl;
+  print(ifs);
+  print(ifs);
+  print(ifs);
+
+  return 0;
+}
+
+void print(std::ifstream& ifs) {
+  char word = ifs.get();
+  std::cout << ifs.tellg() << ": word \"" << word << "\"" << std::endl;
+}
