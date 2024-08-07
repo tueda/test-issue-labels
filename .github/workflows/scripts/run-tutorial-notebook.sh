9095c70254f4e1790dda42fd8a5269b147cbaf6d
Takahiro Ueda <t.ueda.od@juntendo.ac.jp>
2024-08-07 22:19:14 +0900

    refactor: move scripts

--- .github/workflows/scripts/run-tutorial-notebook.sh
+++ .github/workflows/scripts/run-tutorial-notebook.sh
@@ -1,6 +1,8 @@
+# NOTE: this is intended to be invoked within a container.
 set -eu
+notebook_file=$1
 apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
   python3-pip \
   && rm -rf /var/lib/apt/lists/*
 python3 -m pip install --no-cache-dir papermill
-papermill "$1" "$1"
+papermill "$notebook_file" "$notebook_file"
